04d805526393a55e8e4977e32517581038d5a5e8
M-Leprohon <marc@Marc-Andres-MacBook-Pro.local>
2025-02-11 12:59:04 +0200

    Removed prisma

--- src/components/words/WordList.tsx
+++ src/components/words/WordList.tsx
@@ -1,24 +1,28 @@
-import type { Word } from '@prisma/client'
 import Link from 'next/link';
 
 export const WordList = (props: any) => {
-
   const renderable = props.words?.map((word: any, i: number) => {
-    const url = `/word/${word.original_word}`
-    return <div className="block" key={i}>
-      <div className="inline-block w-2/5">
-        <Link className="underline decoration-solid" href={url}>{ word.original_word }</Link>
+    const url = `/word/${word.original_word}`;
+    return (
+      <div className="block" key={i}>
+        <div className="inline-block w-2/5">
+          <Link className="underline decoration-solid" href={url}>
+            {word.original_word}
+          </Link>
+        </div>
+        <div className="inline-block w-2/5">
+          <Link className="underline decoration-solid" href={url}>
+            {word.translated_word}
+          </Link>
+        </div>
+        <div className="inline-block w-1/5">
+          <Link className="underline decoration-solid" href={url}>
+            {word.familiarity}
+          </Link>
+        </div>
       </div>
-      <div className="inline-block w-2/5">
-        <Link className="underline decoration-solid" href={url}>{ word.translated_word }</Link>
-      </div>
-      <div className="inline-block w-1/5">
-        <Link className="underline decoration-solid" href={url}>{ word.familiarity }</Link>
-      </div>
-      </div>
+    );
   });
 
-  return (
-    <div className="">{renderable}</div>
-  );
-}
+  return <div className="">{renderable}</div>;
+};
