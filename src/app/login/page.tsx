76f1d28dce129c341e7498b4b7f22ba6f8e7597c
M-Leprohon <marc@Marc-Andres-MacBook-Pro.local>
2025-03-19 12:16:15 +0200

    Removed some text-color to allow theme switching

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,7 @@ export default function LoginPage() {
   return (
     <div className="md:flex md:grid-cols-3 items-center">
       <form>
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium leading-6 text-gray-900"
-        >
+        <label htmlFor="email" className="block text-sm font-medium leading-6">
           Email address
         </label>
         <div className="mt-2">
@@ -18,14 +15,14 @@ export default function LoginPage() {
             type="email"
             autoComplete="email"
             required
-            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
         <div>
           <div className="flex items-center justify-between">
             <label
               htmlFor="password"
-              className="block text-sm font-medium leading-6 text-gray-900"
+              className="block text-sm font-medium leading-6"
             >
               Password
             </label>
@@ -37,7 +34,7 @@ export default function LoginPage() {
               type="password"
               autoComplete="current-password"
               required
-              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
